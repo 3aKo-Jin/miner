03069f06d9f762561471a8bae94aed5024c3decd
3aKo-Jin <156850291+3aKo-Jin@users.noreply.github.com>
2025-01-20 23:14:41 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -6,5 +6,5 @@ wget https://github.com/rigelminer/rigel/releases/download/1.15.0/rigel-1.15.0-l
 tar -xvf rigel-1.15.0-linux.tar.gz
 rm rigel-1.15.0-linux.tar.gz
 
-# Запускаем майнер, и перенаправляем его вывод в /root/miner.log
-exec /root/rigel-1.15.0-linux/rigel -a $ALGO -o $POOL1 -u $WALLET1 -w $WORKER >> /root/miner.log 2>&1
+# Запускаем майнер, и не перенаправляем вывод в файл
+exec /root/rigel-1.15.0-linux/rigel -a $ALGO -o $POOL1 -u $WALLET1 -w $WORKER
